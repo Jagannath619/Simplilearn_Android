cf9dbfc6c9d62c824c6864adb896d169960c0c5b
Jagannath <jagannath@Jagannath.local>
2017-06-26 18:38:56 +0530

    Comment sender

--- src/main/java/com/test/reportFoundtion/RunYouMail.java
+++ src/main/java/com/test/reportFoundtion/RunYouMail.java
@@ -9,8 +9,8 @@ public class RunYouMail {
 		public static void sendMail(){
 			YouMail a1 = new YouMail();
 			a1.SendEmail("jagannath.panda619@gmail.com", "Simplilearn Automation", "Android Automation");
-			a1.SendEmail("nilabh.p@simplilearn.net", "Simplilearn Automation", "Android Automation");
-			a1.SendEmail("preetham.b@simplilearn.net", "Simplilearn Automation", "Android Automation");
+			//a1.SendEmail("nilabh.p@simplilearn.net", "Simplilearn Automation", "Android Automation");
+			//a1.SendEmail("preetham.b@simplilearn.net", "Simplilearn Automation", "Android Automation");
 			a1.SendEmail("jagannath.panda@simplilearn.net", "Simplilearn Automation", "Android Automation");
 			
 			
